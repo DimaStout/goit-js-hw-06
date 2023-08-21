f71dbf37ba618d2c71c69ea14f512f99a52f16d2
Dmytro Zinchuk <127327533+dmytro2104z@users.noreply.github.com>
2023-08-21 12:27:07 +0200

    hw-6.1.

---
+++ js/task-07.js
@@ -0,0 +1,5 @@
+const fontSizeControlerRef = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+fontSizeControlerRef.addEventListener("input", () => {
+  text.style.fontSize = `${fontSizeControlerRef.value}px`;
+});
